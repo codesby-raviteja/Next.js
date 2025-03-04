494ff2cf8c85b52fab692183b3cb96c1735c6ccd
codesby-raviteja <raviteja555@gmail.com>
2025-03-04 23:34:50 +0530

    inital commit of Day-4

---
+++ Day-4-Dynamic-Routing/app/services/design-services/page.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Designservicespage() {
+  return <h1>welcome to Design services pages</h1>
+}
+
+export default Designservicespage
---
+++ Day-4-Dynamic-Routing/app/services/seo-services/page.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Seoservicespage() {
+  return <h1>welcome to Seo services pages</h1>
+}
+
+export default Seoservicespage
