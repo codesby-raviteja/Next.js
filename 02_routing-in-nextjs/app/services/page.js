ca915b056f1991970e4e6aea00ec2e7442647c69
codesby-raviteja <raviteja555@gmail.com>
2025-03-04 10:38:04 +0530

    initial commit

---
+++ 02_routing-in-nextjs/app/services/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import React from "react"
+
+export default function Services() {
+  return (
+    <>
+      <h1>Welcome to Services Page</h1>
+      <Link href="/">Home page </Link>
+    </>
+  )
+}
